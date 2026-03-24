7bf3005f67ed634260f4e0a93d6af8b725d56d3c
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2026-03-24 15:58:39 -0300

    Criação dos filtros para consulta das parcelas.

--- front-gestao-financiamento/src/app/components/tabela-parcelas/tabela-parcelas.component.ts
+++ front-gestao-financiamento/src/app/components/tabela-parcelas/tabela-parcelas.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ParcelasService } from '../../parcelas-service';
 import { ParcelasModel } from '../../models/parcelas.model';
-import { CommonModule } from '@angular/common'; // Importe o CommonModule
+import { CommonModule } from '@angular/common'; 
 import { ParcelaComponent } from '../parcela/parcela.component';
 
 @Component({
@@ -14,15 +13,56 @@ import { ParcelaComponent } from '../parcela/parcela.component';
 })
 export class TabelaParcelasComponent implements OnInit {
   parcelas: ParcelasModel[] = [];
+  
+  // Novas variáveis para os filtros
+  parcelasFiltradas: ParcelasModel[] = [];
+  datasDisponiveis: string[] = [];
+  
+  // Estado dos filtros
+  filtroStatus: string = '';
+  filtroNumero: string = '';
+  filtroData: string = '';
 
-  constructor(private service: ParcelasService, private cdr: ChangeDetectorRef) {}
+  // Usando o construtor conforme solicitado
+  constructor(
+    private service: ParcelasService, 
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-    this.service.retornaParcelas().subscribe(parcelas=>{
-      this.parcelas = parcelas
-      console.log(this.parcelas)
-      this.cdr.detectChanges()
-    })
+    this.service.retornaParcelas().subscribe(parcelas => {
+      this.parcelas = parcelas;
+      this.parcelasFiltradas = parcelas; // Inicialmente mostra tudo
+      
+      this.gerarOpcoesDeData();
+      
+      console.log(this.parcelas);
+      this.cdr.detectChanges();
+    });
+  }
+
+  // Extrai os meses únicos para o select
+  private gerarOpcoesDeData() {
+    const meses = this.parcelas.map(p => p.MesVencimento);
+    this.datasDisponiveis = [...new Set(meses)];
+  }
+
+  // Função que aplica todos os filtros juntos
+  aplicarFiltros(event: any, tipo: string) {
+    const valor = event.target.value;
+
+    if (tipo === 'status') this.filtroStatus = valor;
+    if (tipo === 'numero') this.filtroNumero = valor;
+    if (tipo === 'data') this.filtroData = valor;
+
+    this.parcelasFiltradas = this.parcelas.filter(p => {
+      const matchStatus = !this.filtroStatus || p.Situacao.toLowerCase() === this.filtroStatus.toLowerCase();
+      const matchNumero = !this.filtroNumero || p.NumeroParcela.toString().includes(this.filtroNumero);
+      const matchData = !this.filtroData || p.MesVencimento === this.filtroData;
+
+      return matchStatus && matchNumero && matchData;
+    });
+
+    this.cdr.detectChanges(); // Garante que a tela atualize após filtrar
   }
 }
-
