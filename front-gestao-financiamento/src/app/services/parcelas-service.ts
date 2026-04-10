4424854685d23017329871de9b0b8b10fb14b83e
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2026-04-10 17:52:54 -0300

    movido servicos para pasta services

--- front-gestao-financiamento/src/app/services/parcelas-service.ts
+++ front-gestao-financiamento/src/app/services/parcelas-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ParcelasModel } from './models/parcelas.model';
+import { ParcelasModel } from '../models/parcelas.model';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,8 @@ export class ParcelasService {
   constructor(private http: HttpClient) {}
   retornaParcelas() {
     return this.http.get<ParcelasModel[]>('http://127.0.0.1:5000/parcelas')
+  }
+  atualizarParcela(id: number, dados: any): Observable<any> {
+    return this.http.put(`http://127.0.0.1:5000/atualizar-parcela/${id}`, dados);
   }
 }
